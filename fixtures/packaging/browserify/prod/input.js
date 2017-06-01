32c3ac06260ed09225504f096c742f8da02cc15d
Luke Sheard <me@lukesheard.com>
2017-06-01 21:14:35 +0100

    Fix browserify build

--- fixtures/packaging/browserify/prod/input.js
+++ fixtures/packaging/browserify/prod/input.js
@@ -2,6 +2,6 @@ var Inferno = require('inferno');
 var createElement = require('inferno-create-element');
 
 Inferno.render(
-	Inferno.createElement('h1', null, 'Hello World!'),
+	createElement('h1', null, 'Hello World!'),
 	document.getElementById('container')
 );
